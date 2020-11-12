3b1f61446c3fcf2f0ad003b5b13961f14f54a515
Juyeong <landsofg@gmail.com>
2020-11-11 23:37:45 -0800

    added

--- sketch.js
+++ sketch.js
@@ -8,6 +8,14 @@ let points = 0;
 let w = 600;
 let h = 600;
 let player;
+let coins = [];
+let playerImg;
+let coinImg;
+
+function preload(){
+  playerImg = loadImage('assets/something/something_100/something_0.png');
+  coinImg = loadImage('assets/ball/ball_1/ball_0.png');
+}
 
 function setup() {
   cnv = createCanvas(w, h);
@@ -16,27 +24,43 @@ function setup() {
 
   player = new Player();
 
+  // coins[0] = new Coin();
+  coins.push(new Coin());
 }
 
 function draw() {
 
-  switch (state){
-  case 'title':
-    title();
-    cnv.mouseClicked(titleMouseClicked);
-    break;
-  case 'level 1':
-    level1();
-    cnv.mouseClicked(level1Mouseclicked);
-    break;
-  case 'you win':
-    youWin();
-    cnv.mouseClicked(youWinMouseClicked);
-    break;
-  default:
-    break;
+  switch (state) {
+    case 'title':
+      title();
+      cnv.mouseClicked(titleMouseClicked);
+      break;
+    case 'level 1':
+      level1();
+      cnv.mouseClicked(level1Mouseclicked);
+      break;
+    case 'you win':
+      youWin();
+      cnv.mouseClicked(youWinMouseClicked);
+      break;
+    default:
+      break;
   }
 
+}
+
+function keyPressed() {
+  if (keyCode == LEFT_ARROW) {
+    player.direction = 'left'
+  } else if (keyCode == RIGHT_ARROW) {
+    player.direction = 'right'
+  } else if (keyCode == UP_ARROW) {
+    player.direction = 'up'
+  } else if (keyCode == DOWN_ARROW) {
+    player.direction = 'down'
+  } else if (key = ' ') {
+    player.direction = 'still';
+  }
 }
 
 function title() {
@@ -44,11 +68,11 @@ function title() {
   textSize(80);
   fill(255);
   textAlign(CENTER);
-  text("My Game", w/2, h/5);
+  text("My Game", w / 2, h / 5);
 
 
   textSize(30);
-  text('click anywhere to start', w/2, h/2);
+  text('click anywhere to start', w / 2, h / 2);
 }
 
 function titleMouseClicked() {
@@ -57,33 +81,60 @@ function titleMouseClicked() {
 }
 
 function level1() {
-  background(50, 150, 200,);
-  text('click  for  points', w/2, h - 30);
+  background(20, 20, 20, );
+
+  if (random(1) <= 0.01) {
+    coins.push(new Coin());
+  }
 
   player.display();
+  player.move();
+
+  // iterating through coins array to display and move them
+
+  // using for loop
+  for (let i = 0; i < coins.length; i++) {
+    coins[i].display();
+    coins[i].move();
+  }
+
+  // check for collison, if there is a collision increase points by 1 AND splice that coin out of array
+  // need to iterate backwards through array
+  for (let i = coins.length - 1; i >= 0; i--) {
+    if (dist(player.x, player.y, coins[i].x, coins[i].y) <= (player.r + coins[i].r) / 2) {
+      points++;
+      console.log(points);
+      coins.splice(i, 1);
+    } else if (coins[i].y > h){
+      coins.splice(i, 1);
+      console.log('coin is out of town');
+    }
+  }
+
+  text(`Points: ${points}`, w / 5, h - 30);
 }
 
 function level1Mouseclicked() {
-  points++;
-  console.log('points = ' + points);
-
-  if (points >= 10){
-    state = 'you win';
-  }
+  //  points++;
+  //  console.log('points = ' + points);
+  //
+  //  if (points >= 10){
+  //    state = 'you win';
+  //  }
 }
 
-  function youWin(){
-    background(255, 50, 80);
-    textSize(80);
-    stroke(255);
-    text("YOU WIN", w/2, h/2);
+function youWin() {
+  background(255, 50, 80);
+  textSize(80);
+  stroke(255);
+  text("YOU WIN", w / 2, h / 2);
 
-    textSize(30);
-    text('click anywhere to restart', w/2, h * 3/4);
-  }
+  textSize(30);
+  text('click anywhere to restart', w / 2, h * 3 / 4);
+}
 
-  function youWinMouseClicked(){
-    state = 'level 1';
-    points = 0;
+function youWinMouseClicked() {
+  state = 'level 1';
+  points = 0;
 
-  }
+}
